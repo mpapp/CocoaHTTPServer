c18aea2bdb5c587f1141d0fcebfabce8b359ce4f
mz2 <matias.piipari@gmail.com>
2015-12-30 15:10:30 +0200

    Adds a modulemap

--- CocoaHTTPServer/CocoaHTTPServer/CocoaHTTPServer.h
+++ CocoaHTTPServer/CocoaHTTPServer/CocoaHTTPServer.h
@@ -16,4 +16,11 @@ FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CocoaHTTPServer/PublicHeader.h>
 
-
+#import "HTTPConnection.h"
+#import "HTTPMessage.h"
+#import "HTTPResponse.h"
+#import "HTTPServer.h"
+#import "HTTPDataResponse.h"
+#import "HTTPDynamicFileResponse.h"
+#import "HTTPFileResponse.h"
+#import "HTTPRedirectResponse.h"
