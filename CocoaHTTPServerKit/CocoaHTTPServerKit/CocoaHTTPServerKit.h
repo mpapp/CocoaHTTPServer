e482f797dea6de1a302d62495bfaa4869a0bb8e4
Matias Piipari <matias.piipari@gmail.com>
2016-10-28 11:21:47 +0100

    Adds missing stuff to the umbrella header.

--- CocoaHTTPServerKit/CocoaHTTPServerKit/CocoaHTTPServerKit.h
+++ CocoaHTTPServerKit/CocoaHTTPServerKit/CocoaHTTPServerKit.h
@@ -16,4 +16,22 @@ FOUNDATION_EXPORT const unsigned char CocoaHTTPServerKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CocoaHTTPServerKit/PublicHeader.h>
 
-
+#import "DDData.h"
+#import "DDNumber.h"
+#import "DDRange.h"
+#import "MultipartFormDataParser.h"
+#import "MultipartMessageHeader.h"
+#import "MultipartMessageHeaderField.h"
+#import "HTTPAsyncFileResponse.h"
+#import "HTTPDataResponse.h"
+#import "HTTPDynamicFileResponse.h"
+#import "HTTPFileResponse.h"
+#import "HTTPRedirectResponse.h"
+#import "GCDAsyncSocket.h"
+#import "WebSocket.h"
+#import "HTTPAuthenticationRequest.h"
+#import "HTTPConnection.h"
+#import "HTTPLogging.h"
+#import "HTTPMessage.h"
+#import "HTTPResponse.h"
+#import "HTTPServer.h"
