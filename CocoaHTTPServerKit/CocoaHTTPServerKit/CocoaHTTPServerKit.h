1a83826171c3deb1ee146e23aa6d239382324856
Matias Piipari <matias.piipari@gmail.com>
2016-10-28 10:40:20 +0100

    Adds a framework target.

---
+++ CocoaHTTPServerKit/CocoaHTTPServerKit/CocoaHTTPServerKit.h
@@ -0,0 +1,19 @@
+//
+//  CocoaHTTPServerKit.h
+//  CocoaHTTPServerKit
+//
+//  Created by Matias Piipari on 28/10/2016.
+//  Copyright © 2016 Manuscripts.app Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CocoaHTTPServerKit.
+FOUNDATION_EXPORT double CocoaHTTPServerKitVersionNumber;
+
+//! Project version string for CocoaHTTPServerKit.
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaHTTPServerKit/PublicHeader.h>
+
+
